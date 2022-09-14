a854ce4fadaaec4c734bb8a69ba435b0cacedfa4
Surya Bhusal <surya@fetch.ly>
2022-09-14 08:20:19 +0545

    Tasks CRUD controllers setup.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,59 @@
 class TasksController < ApplicationController
+  before_action :set_task, only [:show, :edit, :update, :destroy]
+
   def show
   end
 
   def new
+    @task = Task.new
   end
 
   def edit
   end
+
+  def create
+    @task = Task.new(task_params)
+
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to @task, notice: "Task was created successfully."}
+        format.json { render :show, status: :created, location: @task }
+      else
+        format.html { render :new }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+
+  end
+
+  def update
+    respond_to do |format|
+      if @task.update(task_params)
+        format.html { redirect_to @task, notice: "Task was updated successfully."}
+        format.json { render :show, status: :created, location: @task }
+      else
+        format.html { render :edit }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @task.destroy
+    respond_to do |format|
+      format.html { redirect_to project_url(@task.project_id), notice: "Task was delted successfully."}
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+    def set_task
+      # TODO
+    end
+
+    def task_params
+      # TODO
+    end
+
 end
